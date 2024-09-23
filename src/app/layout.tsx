108c6a9b51020285081f37c7f515e1ceaf906514
D_Zucho <danny.nico.dev@outlook.com>
2024-09-23 16:27:48 -0700

    initial + setup

---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import localfont from "next/font/local";
+import { PrismicPreview } from "@prismicio/next";
+import { repositoryName } from "@/prismicio";
+
+import "./app.css";
+
+const alpino = localfont({
+  src: "../../public/fonts/Alpino-Variable.woff2",
+  display: "swap",
+  weight: "100 700",
+  variable: "--font-alpino",
+});
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className={alpino.variable}>
+      <body>{children}</body>
+      <PrismicPreview repositoryName={repositoryName} />
+    </html>
+  );
+}
